7f1f1c46e61b90ac895231d471f4cf06b705f4df
Aljondt <104638631+aljondetorres@users.noreply.github.com>
2023-09-22 13:09:43 +0800

    update 1

---
+++ index.js
@@ -0,0 +1,36 @@
+// let name = "Aljon"
+// let surname = "De Torres"
+
+// let fullName = name +" "+ surname
+    
+// console.log(fullName)
+
+// let name = "ALJON"
+// let greeting = "Hi Pogi"
+
+// function greetingPogi() {
+//     console.log( greeting + ", " + name + "!") 
+// }
+// greetingPogi()
+
+// let myPoints = 3
+
+// function plus3() {
+//     myPoints += 3
+
+
+// }
+// function minus1() {
+//     myPoints -= 1
+
+// }
+
+
+let error1 = document.getElementById("error")
+
+
+function purchase1() {
+    error1.textContent = "Something went wrong, please try again"
+    console.log("botton Clicked")
+}
+
